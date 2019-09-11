d4b45cdb0577b8dcde6ef24b1b27d1b5b0704e1d
Richard Lee <richlee81192@gmail.com>
2019-09-11 17:10:48 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,6 +17,7 @@ WIN_COMBINATIONS = [
   
   def won?(board)
     WIN_COMBINATIONS.each do |win_combinations|
+    
     win_index_1 = WIN_COMBINATIONS[0]
     win_index_2 = WIN_COMBINATIONS[1]
     win_index_3 = WIN_COMBINATIONS[2]
