d6100d8f2017a38f3a61a3c1eec051dd8614acf2
Richard Lee <richlee81192@gmail.com>
2019-09-11 16:53:33 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -38,7 +38,7 @@ WIN_COMBINATIONS = [
     position_9 = board[win_index_9]
     
   if position_1 == 'X' && position_2 == 'X' && position_3 == 'X'
-    return win_index_1
+    return WIN_COMBINATIONS
   else
     false
   end
