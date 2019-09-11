1154311dac8ac82d88e82c85bf13d86c9460de1d
Richard Lee <richlee81192@gmail.com>
2019-09-11 17:13:36 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,15 +19,15 @@ WIN_COMBINATIONS = [
     
     WIN_COMBINATIONS.each do |win_combinations|
     
-    win_index_1 = WIN_COMBINATIONS[0]
-    win_index_2 = WIN_COMBINATIONS[1]
-    win_index_3 = WIN_COMBINATIONS[2]
-    win_index_4 = WIN_COMBINATIONS[3]
-    win_index_5 = WIN_COMBINATIONS[4]
-    win_index_6 = WIN_COMBINATIONS[5]
-    win_index_7 = WIN_COMBINATIONS[6]
-    win_index_8 = WIN_COMBINATIONS[7]
-    win_index_9 = WIN_COMBINATIONS[8]
+    win_index_1 = win_combinations[0]
+    win_index_2 = win_combinations[1]
+    win_index_3 = win_combinations[2]
+    win_index_4 = win_combinations[3]
+    win_index_5 = win_combinations[4]
+    win_index_6 = win_combinations[5]
+    win_index_7 = win_combinations[6]
+    win_index_8 = win_combinations[7]
+    win_index_9 = win_combinations[8]
     
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
